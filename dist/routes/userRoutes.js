ff16a4ef265629c7d1cc55490ba71c0803bf3235
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-06-13 23:55:04 +0100

    Edited dist folder

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -7,4 +7,7 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const userController_1 = require("../controller/userController");
 router.post('/auth', userController_1.authUser);
+router.post('/auth', userController_1.authUser);
+router.post('/logout', userController_1.logOutUser);
+router.route('/profile').get(userController_1.getUserProfile).put(userController_1.updateUserProfile);
 exports.default = router;
