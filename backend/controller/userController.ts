71f9a30382e91a5cf8b1c15d558cfad5b475c186
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-06-13 23:48:38 +0100

    Setup user controllers

--- backend/controller/userController.ts
+++ backend/controller/userController.ts
@@ -1,16 +1,44 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import asyncHandler from 'express-async-handler'
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
-interface authUserProps {
-    req: Request,
-    res: Response
-}
-
-const authUser = ({req, res}: authUserProps) => {
+const authUser = asyncHandler(async (req: Request, res: Response) => {
     res.status(200).json({ message: 'Auth User' })
-}
+})
 
-export { authUser }
+// @desc Register a new user
+// route POST /api/users/
+// @access Public
+
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Register User' })
+})
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+
+const logOutUser = asyncHandler(async (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Logout User' })
+})
+
+// @desc Get user profile
+// route GET /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Register User' })
+})
+ 
+// @desc Update user profile
+// route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Update user profile' })
+})
+
+export { authUser, registerUser, logOutUser, getUserProfile, updateUserProfile } 
