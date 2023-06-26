4ee21dda97aa742e4db8313465dbb8554baa3036
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-06-26 23:14:07 +0100

    Added logoutUser controller

--- backend/controller/userController.ts
+++ backend/controller/userController.ts
@@ -8,65 +8,68 @@ import User from "../models/userModel";
 // @access Public
 
 const authUser = asyncHandler(async (req: Request, res: Response) => {
-    const { email, password } = req.body
-
-    const user = await User.findOne({ email })
-
-    if (user) {
-        generateToken(res, user._id)
-        res.status(201).json({
-           _id: user._id,
-           name: user.name,
-           email: user.email,
-        })
+    const { email, password } = req.body;
+  
+    const user = await User.findOne({ email });
+  
+    if (user && (await user.matchPassword(password))) {
+      generateToken(res, user._id);
+  
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
     } else {
-        res.status(401)
-        throw new Error('Invalid email or passsword')
+      res.status(401);
+      throw new Error('Invalid email or password');
     }
-
-
-})
+  });
 
 // @desc Register a new user
 // route POST /api/users/
 // @access Public
 
 const registerUser = asyncHandler(async (req: Request, res: Response) => {
-    const { name, email, password } = req.body
+    const { name, email, password } = req.body;
 
-    const userExists = await User.findOne({ email})
+    const userExists = await User.findOne({ email });
 
     if (userExists) {
-        res.status(400)
-        throw new Error('User already exists')
+        res.status(400).json({ error: 'User already exists' });
+        return;
     }
-    
+
     const user = await User.create({
         name,
         email,
         password
-    })
-    
+    });
+
     if (user) {
-        generateToken(res, user._id)
+        generateToken(res, user._id);
         res.status(201).json({
            _id: user._id,
            name: user.name,
            email: user.email,
-        })
+        });
     } else {
-        res.status(400)
-        throw new Error('Invalid user data')
+        res.status(400).json({ error: 'Invalid user data' });
     }
+});
 
-})
 
 // @desc Logout user
 // route POST /api/users/logout
 // @access Public
 
 const logOutUser = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({ message: 'log out user' })
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    })
+
+    res.status(200).json({ message: 'User logged out'})
 })
 
 // @desc Get user profile
