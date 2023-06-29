3c80c1c0991ef4ab3f32c5064ccd9aaf0de3a19e
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-06-29 19:34:39 +0100

    Setup auth slice

---
+++ frontend/src/slices/authSlices.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+
+interface UserInfo {
+    userInfo: unknown | null
+}
+
+const storedUserInfo = localStorage.getItem('userInfo')
+const initialState: UserInfo = {
+    userInfo: storedUserInfo ? JSON.parse(storedUserInfo) : null
+}
+
+const authslice = createSlice({
+   name: 'auth',
+   initialState,
+   reducers: {
+     setCredentials: (state, action: PayloadAction<UserInfo>) => {
+        state.userInfo = action.payload;
+        localStorage.setItem('userInfo', JSON.stringify(action.payload))
+     },
+     clearCredentials: (state, action: PayloadAction<UserInfo>) => {
+        state.userInfo = null
+        localStorage.removeItem('userInfo')
+     }
+   }
+})
+
+export const { setCredentials, clearCredentials } = authslice.actions
+
+export default authslice.reducer
