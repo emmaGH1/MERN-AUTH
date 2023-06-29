5221b21296511fb38f027b03f6c4ab6fff4daf66
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-06-29 18:46:40 +0100

    Setup redux store

---
+++ frontend/src/store.ts
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const store = configureStore({
+   reducer: {},
+   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+   devTools: true
+}) 
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store;
+
+
+
+
+
+
+
+
+
